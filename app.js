c2b179ab075e32288368df14836fe016327c5ce7
ajshopov <alexanderjshopov@gmail.com>
2018-02-27 00:39:23 +1100

    passport js oauth2 strava working for login

--- app.js
+++ app.js
@@ -1,25 +1,65 @@
 var express = require('express')
 var passport = require('passport')
+// var util = require('util')
+var StravaStrategy = require('passport-strava-oauth2').Strategy;
 var app = express()
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 var strava = require('strava-v3');
-var request = require('request');
-let cors = require('cors')
-app.use(cors());
+// var request = require('request');
+// let cors = require('cors')
+// app.use(cors());
 
 var STRAVA_ACCESS_TOKEN = process.env.STRAVA_ACCESS_TOKEN;
 var STRAVA_CLIENT_ID = process.env.STRAVA_CLIENT_ID
 var STRAVA_CLIENT_SECRET = process.env.STRAVA_CLIENT_SECRET
 var STRAVA_REDIRECT_URI = process.env.STRAVA_REDIRECT_URI
 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
 app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
+// var cookie = require('cookie');
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 var appData;
 
+var userToken;
 
-strava.oauth.getRequestAccessURL({STRAVA_ACCESS_TOKEN, STRAVA_CLIENT_SECRET, STRAVA_CLIENT_ID, STRAVA_REDIRECT_URI})
+
+passport.use(new StravaStrategy({
+    clientID: STRAVA_CLIENT_ID,
+    clientSecret: STRAVA_CLIENT_SECRET,
+    callbackURL: "http://127.0.0.1:3000/auth/strava/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      
+      // To keep the example simple, the user's Strava profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Strava account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
+
+
+
+
+
+// strava.oauth.getRequestAccessURL({STRAVA_ACCESS_TOKEN, STRAVA_CLIENT_SECRET, STRAVA_CLIENT_ID, STRAVA_REDIRECT_URI})
 
 
 
@@ -33,26 +73,69 @@ strava.oauth.getRequestAccessURL({STRAVA_ACCESS_TOKEN, STRAVA_CLIENT_SECRET, STR
 
 
 app.get('/', function (req, res) {
-  res.render('splash')
+  res.render('splash', { user: req.user })
 })
 
 
-app.get('/home', function (req, res) {
-  strava.athletes.get({id: 26705652},function(err,payload,limits) {
-    //do something with your payload, track rate limits
-    console.log(err)
-    console.log(limits)
-    console.log(payload)
-    appData = payload
+ 
+// GET /auth/strava
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Strava authentication will involve
+//   redirecting the user to strava.com.  After authorization, Strava
+//   will redirect the user back to this application at /auth/strava/callback
+
+app.get('/auth/strava',
+  passport.authenticate('strava', { scope: ['public'] }),
+  function(req, res){
+    // The request will be redirected to Strava for authentication, so this
+    // function will not be called.
+  });
+
+// GET /auth/strava/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/strava/callback', 
+  passport.authenticate('strava', { failureRedirect: '/' }),
+  function(req, res) {
+    res.redirect('/home');
   });
 
 
-  res.render('home', {
-    appData: appData
-  })
+
+app.get('/home', ensureAuthenticated, function (req, res) {
+  // strava.athletes.get({id: 26705652},function(err,payload,limits) {
+  //   //do something with your payload, track rate limits
+  //   console.log(err)
+  //   console.log(limits)
+  //   console.log(payload)
+  //   appData = payload
+  // });
+
+  res.render('home',{ user: req.user })
+  // console.log(req.user)
   // res.json(appData)
 })
- 
+
+
+app.get('/json', ensureAuthenticated, function(req,res){
+  var args = {
+    id:1424416003, 'access_token':STRAVA_ACCESS_TOKEN}
+    strava.activities.get(args,function(err,payload,limits) {
+    console.log(err)
+    console.log(payload)
+    console.log(limits)
+     //do something with your payload, track rate limits 
+ });
+  // console.log(req.user)
+  res.json(req.user)
+})
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
 
   // fetch('https://www.strava.com/oauth/authorize', function(req, res){
   //       res.json({
@@ -63,6 +146,12 @@ app.get('/home', function (req, res) {
   //       })
   // })
 
-app.listen(PORT, function(){
-  console.log(`listening on port ${PORT}`);
+app.listen(3000, function(){
+  console.log(`listening on port 3000`);
 }); 
+
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/')
+}
