0b348e1f68bdf0ade9b1f14e180c441ea61dac40
ajshopov <alexanderjshopov@gmail.com>
2018-03-01 10:18:06 +1100

    redirect to new url

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ var accessTkn;
 passport.use(new StravaStrategy({
     clientID: STRAVA_CLIENT_ID,
     clientSecret: STRAVA_CLIENT_SECRET,
-    callbackURL: "http://127.0.0.1:3000/auth/strava/callback"
+    callbackURL: "http://therunningman.herokuapp.com/auth/strava/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     console.log(accessToken)
